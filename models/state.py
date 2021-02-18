6f36932d0a0c8e91ebb3eeecb33ef290ed583027
leolunac-holbertons <2176@leolunac-Holbertons>
2021-02-18 01:39:52 +0000

    Write all those classes that inherit from BaseModel

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module containing the ``State`` class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        The ``State`` class which inherits from ``BaseModel`` class.
+    """
+    name = ""
