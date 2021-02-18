2cce719a58d5b0563ff25720a803e8de45ba8356
leolunac-holbertons <2176@leolunac-Holbertons>
2021-02-18 01:37:26 +0000

    Write all those classes that inherit from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Module containing the ``Amenity`` class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        The ``Amenity`` class which inherits from ``BaseModel`` class.
+    """
+    name = ""
